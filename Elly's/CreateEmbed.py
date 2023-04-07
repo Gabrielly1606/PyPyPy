7ea6f4267725db443bdc6dcd8e7e8e56001c56f9
Gabrielly1606 <gabriellydasilva1606@gmail.com>
2023-04-07 00:40:46 -0300

    New CreateEmebed

--- Elly's/CreateEmbed.py
+++ Elly's/CreateEmbed.py
@@ -1,12 +1,8 @@
 import discord; from discord.ext import commands
 
 class MyCog(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        
-class Modal(discord.ui.Modal):
     def __init__(self):
-        super(). __initt__(title="Anuncie sua mesa!")
+        super(). __init__(title="Anuncie sua mesa!")
         
         titulo = discord.ui.TextInput(label='Título da Embed:', style=discord.TextStyle.short)
         descrição = discord.ui.TextInput(label='Descição da Embed:', style=discord.TextStyle.short)
@@ -15,10 +11,7 @@ class Modal(discord.ui.Modal):
             embed = discord.Embed(title=self.titulo, description=self.descrição, color=discord.Color.blurple())
             await interaction.respons.send_message('Mesa anunciada com sucesso!', ephemeral=True)
             await interaction.channel.send(embed=embed)
-
-class CreateEmbed(commands.Cog):
-     def __init__(self, bot):
-        self.bot = bot
+            
 
 
    
