51608d7e934d68d3ed97ed403bcd1390a0fb488b
cosmo <cosmoskitsune@hotmail.com>
2023-04-23 16:44:01 -0300

    update?

--- Elly's/CreateEmbed.py
+++ Elly's/CreateEmbed.py
@@ -11,7 +11,6 @@ class MyCog(commands.Cog, discord.ui.Modal):
             embed = discord.Embed(title=self.titulo, description=self.descricao, color=discord.Color.blurple())
             await interaction.respons.send_message('Mesa anunciada com sucesso!', ephemeral=True)
             await interaction.channel.send(embed=embed)
-            
 
 
    
